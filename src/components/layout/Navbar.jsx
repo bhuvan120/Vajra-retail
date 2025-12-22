b3828c8716cfb21b5f47f6713db95e604f27fe33
Karanam Bhuvanesh <karanambhuvanesh@gmail.com>
2025-12-22 15:41:29 +0530

    responsive

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,78 +1,92 @@
-import React from "react";
-import Login from "../../pages/Login";
+import React, { useState } from "react";
 import "./Navbar.css";
-import { Link } from "react-router-dom";
-import About from "../../pages/About";
-
+import { NavLink, Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const closeMenu = () => setMenuOpen(false);
+
   return (
     <header>
       <nav className="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
         <div className="container">
 
           {/* Logo */}
-          <a className="navbar-brand" href="/">
+          <Link className="navbar-brand" to="/" onClick={closeMenu}>
             <img src="image.png" alt="Vajra Logo" className="navbar-logo" />
-          </a>
+          </Link>
 
-          {/* Mobile Toggle */}
+          {/* Toggle Button */}
           <button
             className="navbar-toggler"
             type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#mainNavbar"
-            aria-controls="mainNavbar"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
+            onClick={() => setMenuOpen(!menuOpen)}
           >
             <span className="navbar-toggler-icon"></span>
           </button>
 
           {/* Menu */}
-          <div className="collapse navbar-collapse" id="mainNavbar">
+          <div className={`collapse navbar-collapse ${menuOpen ? "show" : ""}`}>
             <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+
               <li className="nav-item">
-        
-                <a className="nav-link " href="/">Home</a>
+                <NavLink
+                  to="/"
+                  onClick={closeMenu}
+                  className={({ isActive }) =>
+                    isActive ? "nav-link active-link" : "nav-link"
+                  }
+                >
+                  Home
+                </NavLink>
               </li>
+
               <li className="nav-item">
-                {/* <Link to="../../pages/About">aAbout</Link> */}
-                <a className="nav-link" href="/about">About</a>
+                <NavLink
+                  to="/about"
+                  onClick={closeMenu}
+                  className={({ isActive }) =>
+                    isActive ? "nav-link active-link" : "nav-link"
+                  }
+                >
+                  About
+                </NavLink>
               </li>
+
               <li className="nav-item">
-                <a className="nav-link" href="/contact">Contact</a>
+                <NavLink
+                  to="/contact"
+                  onClick={closeMenu}
+                  className={({ isActive }) =>
+                    isActive ? "nav-link active-link" : "nav-link"
+                  }
+                >
+                  Contact
+                </NavLink>
               </li>
             </ul>
 
-            {/* Cart + Auth */}
+            {/* Auth Buttons */}
             <div className="navbar-actions">
-              
-              {/* Cart */}
-              {/* <div className="cart-icon me-3">
-                <span className="cart-count">0</span>
-                 🛒 
-              </div> */}
-
-              {/* Auth Buttons */}
-              {/* <button className="btn btn-outline-primary fw-bold me-2">
-                Sign Up
-              </button> */}
-
-              <Link to="/signup" className="btn btn-outline-primary fw-bold me-2">
+              <Link
+                to="/signup"
+                className="btn btn-outline-primary fw-bold me-2"
+                onClick={closeMenu}
+              >
                 Sign Up
               </Link>
 
-              {/* <button className="btn btn-primary fw-bold">
-                Log In
-              </button> */}
-
-              <Link to="/login" className="btn btn-primary fw-bold">
+              <Link
+                to="/login"
+                className="btn btn-primary fw-bold"
+                onClick={closeMenu}
+              >
                 Log In
               </Link>
-
             </div>
           </div>
+
         </div>
       </nav>
     </header>
